a5c482c50d124902ed4308e47ac609652e19a3e9
RK <mhgeoe@gmail.com>
2020-10-25 21:06:50 +0900

    Euclidian

--- problem_generator.py
+++ problem_generator.py
@@ -1,9 +1,14 @@
-import VRPTW_util as util
+import argparse
+import itertools
+import math
 import random
-import sys
+
+import VRPTW_util as util
 
 
-def generate_problem(num_nodes: int, num_vehicles: int, lb_distance: float, ub_distance: float, tw_variance=3.0) -> util.VRPTWProblem:
+def generate_problem(name: str, num_vehicles: int, x_limit: int, y_limit: int,
+                     num_nodes: int, tw_limit_ratio: float,
+                     seed: int = 1234) -> util.VRPTWInstance:
     """
     Generates a VRPTW Problem with the given parameters.
 
@@ -16,69 +21,71 @@ def generate_problem(num_nodes: int, num_vehicles: int, lb_distance: float, ub_d
     :param tw_variance: the factor indicates how wide can the time window be in terms of multiple of ub_distance
     :return: a well defined VRPTW Problem.
     """
-    random.seed(1234)
+    assert num_vehicles > 0 and x_limit >= 0 and y_limit >= 0 and num_nodes > 0
+    assert (x_limit + 1) * (y_limit + 1) >= num_nodes
 
-    nodes = []
-    for i in range(num_nodes):
-        nodes.append(util.Node(i))
+    random.seed(seed)
 
-    dt = util.DistanceTable(num_nodes)
-    for i in range(num_nodes):
-        for j in range(i+1, num_nodes):
-            dt.update_distance(nodes[i], nodes[j], random.uniform(lb_distance, ub_distance))
+    all_x_y = list(itertools.product(range(x_limit + 1), range(y_limit + 1)))
+    x_y_list = random.sample(all_x_y, num_nodes)
+    nodes = [util.Node(i, t[0], t[1]) for i, t in enumerate(x_y_list)]
 
-    # To make sure the problem is feasible, we will generate a random route and set the time windows
-    # of tasks to match that route.
-    route = nodes[1:] # the first node is depot
-    random.shuffle(route)
-    distance = 0
-    prev_node = nodes[0]
-    for curr_node in route:
-        distance += dt.get_distance(prev_node, curr_node)
-        curr_node.update_time_window(max(0.0, distance - tw_variance * random.uniform(0,ub_distance)),
-                                     distance + tw_variance * random.uniform(0, ub_distance))
-        prev_node = curr_node
+    # To make sure the problem is feasible, we will generate random routes and set the time windows
+    # of tasks to match those routes.
+    indices = list(range(1, num_nodes))
+    random.shuffle(indices)
+    routes = []
+    start = 0
 
-    return util.VRPTWProblem(num_vehicles, nodes, dt)
+    for i in range(num_vehicles):
+        end = num_nodes
 
+        if i < num_vehicles - 1:
+            end = random.randint(start, num_nodes)
 
-def main(argv) -> util.VRPTWProblem:
-    """
-    Generates a VRPTW problem and possibly print the problem in a file based on user's choice
+        routes.append(indices[start:end])
+        start = end
 
-    Command line usage: problem_generator.py <# of customers> <# of vehicles> -o <output file>
-    The problem won't be displayed without -o flag.
+    tw_limit = int(math.sqrt(x_limit ** 2 + y_limit ** 2) * tw_limit_ratio)
 
-    :param argv: command line arguments
-    :return: A VRPTW problem that is in same size as user's inputs
-    """
-    usage_message = "usage: " + argv[0] + " <# of customers> <# of vehicles> -o <output file>"
-    args = argv[1:]
-    if not (len(args) == 2 or len(args) == 4):
-        print(usage_message)
-        sys.exit(2)
-    try:
-        num_customers = int(args[0])
-        num_vehicles = int(args[1])
-    except ValueError:
-        print(usage_message)
-        sys.exit(2)
+    for r in routes:
+        prev = 0
+        t = 0.0
 
-    lb = random.uniform(10, 100)
-    up = lb * 2
-    problem = generate_problem(num_customers, num_vehicles, lb, up)
+        for cur in r:
+            t += nodes[prev].distance(nodes[cur])
+            a = max(math.floor(t) - random.randint(0, tw_limit), 0)
+            b = math.ceil(t) + random.randint(0, tw_limit)
+            nodes[cur].update_time_window(a, b)
+            prev = cur
 
-    # If user choose to print the problem in some file
-    if len(args) == 4:
-        if args[2] != "-o":
-            print(usage_message)
-            sys.exit(2)
-        f = open(args[3], "w")
-        f.write(str(problem))
-        f.close()
-    return problem
+    return util.VRPTWInstance(name, num_vehicles, nodes), routes
 
 
-if __name__ == "__main__":
-    the_VRPTW = main(sys.argv)
-    print(the_VRPTW)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num-vehicles', '-v', type=int, default=2)
+    parser.add_argument('--x-limit', '-x', type=int, default=10)
+    parser.add_argument('--y-limit', '-y', type=int, default=10)
+    parser.add_argument('--num-customers', '-c', type=int, default=5)
+    parser.add_argument('--tw-limit-ratio', '-t', type=float, default=0.3)
+    parser.add_argument('--name', type=str, default='VRPTW instance')
+    parser.add_argument('--output', '-o', type=str, required=True)
+    parser.add_argument('--solution', '-s', type=str)
+    parser.add_argument('--seed', type=int, default=1234)
+    args = parser.parse_args()
+
+    name = args.name
+    num_vehicles = args.num_vehicles
+    num_customers = args.num_customers
+    x_limit = args.x_limit
+    y_limit = args.y_limit
+    tw_limit_ratio = args.tw_limit_ratio
+    seed = args.seed
+    problem, routes = generate_problem(
+        name, num_vehicles, x_limit, y_limit, num_customers, tw_limit_ratio,
+        seed=seed)
+    problem.dump(args.output)
+
+    if args.solution is not None:
+        util.dump_routes(problem.name, routes, args.solution)
---
+++ validate_solution.py
@@ -0,0 +1,50 @@
+import argparse
+from typing import List
+
+import VRPTW_util as util
+
+
+def load_solution(filename: str) -> List[List[int]]:
+    with open(filename) as f:
+        f.readline()
+        num_vehicles = int(f.readline().rstrip())
+        routes = []
+
+        for _ in range(num_vehicles):
+            routes.append([int(c) for c in f.readline().split()])
+
+    return routes
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--problem', '-p', type=str, required=True)
+    parser.add_argument('--solution', '-s', type=str, required=True)
+    args = parser.parse_args()
+
+    problem = util.VRPTWInstance.load(args.problem)
+    routes = load_solution(args.solution)
+    result = problem.get_time(routes)
+
+    if result is None:
+        print('invalid solution')
+    else:
+        for i, r in enumerate(routes):
+            print('vehicle {}'.format(i))
+            text = '\t0: 0.0'
+            last = -1
+            last_time = 0.0
+
+            for c in r:
+                t = result[1][c][1]
+                text += ', {}: {:.2f}'.format(c, t)
+                last = c
+                last_time = t
+
+            if last != -1:
+                total_time = last_time + problem.distance(last, 0)
+                text += ', 0: {:.2f}'.format(total_time)
+
+            print(text)
+
+        print('total cost: {:.2f}'.format(result[0]))
