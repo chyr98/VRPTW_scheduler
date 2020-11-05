d21aed71103e3e36ab3c8d3e6340f738e0a051c7
Anton Korikov <korikov@mie.utoronto.ca>
2020-11-05 13:02:07 -0500

    finished point selection and evaluation functions

--- LNS.py
+++ LNS.py
@@ -20,13 +20,11 @@ def readPath(filepath,nVeh):
     return path
 
 def makeVisitData(path, cData):
-    #return a list of parameters for each visit in the routes [[customer number, vehichle served by, service time],...]
+    #if the path is feasible, return a list of parameters for each visit in the routes [[customer number, vehichle served by, service time],...]
+    #if the path is infeasible return None
 
     nVeh = len(path)
-    nCus = len(cData)
-
     
-
     all_visits = []
 
     for i in range(nVeh):
@@ -48,7 +46,6 @@ def makeVisitData(path, cData):
             #get service time
             t = t + d
 
-
             #service time is delayed if need to wait for time windows
             tw_start = cData[node_next][2]
             tw_end = cData[node_next][3]
@@ -56,7 +53,9 @@ def makeVisitData(path, cData):
             if tw_start > t:
                 t = tw_start
             elif tw_end < t:
-                print("infeasible: (vehicle, node i, node j)" , (i, node_curr, node_next))
+                return None
+                #Infeasible
+                
 
             info_next = [node_next, i, t]
             all_visits.append(info_next)
@@ -177,8 +176,124 @@ def relatedness(v1,v2,cData):
 
     return r
 
+def GetSTimes(nCus,nVeh,v):
+    #generate the service times for each vehicle
+    #returns [[s_0,1, s_0,2, ..., s_0,(n-1)],...] where s_ij is the time that vehicle i serves customer j 
 
-def run_LNS(frac, bPath, pInfo, nSwaps):
+    res = [[0 for i in range(nCus-1)] for j in range(nVeh)]
+    
+    for i in range(len(v)):
+        cust = v[i][0]
+        veh = v[i][1]
+        s = v[i][2]
+
+        res[veh][cust-1] = s
+
+    return res
+
+def Objective(s_MPP, s_orig):
+    #calculate the MPP objective between the original and the service times
+    obj = 0
+
+    if len(s_MPP) != len(s_orig):
+        print("warning: service time lists for original and new solutions have different lengths")
+
+    for i in range(len(s_MPP)):
+        for j in range(len(s_MPP[i])):
+
+            obj += abs(s_MPP[i][j] - s_orig[i][j])
+            
+
+    return obj
+
+
+def ChooseVisit(r_path, rem_vs, orig_s_times,cData,d):
+    #given a set of removed visits and a reduced path, select the variable (i.e return customer ID) to branch on and return the top d points (only these will be searched)
+
+    #make a list of feasible insertion points for each visit (ind, V, c), inserting the visit at index ind in the route of vehicle v, at cost c
+
+    #find the variable with the maximum minimum cost
+    max_min_cost_tot = 0
+    #print('\n reduced path is ', r_path)
+    #print('\n removed visits are ', rem_vs)
+
+
+    for i in range(len(rem_vs)):
+        f_pts = []
+
+        #print('------------------------------------------considering customer ' , rem_vs[i][0])
+
+        for j in range(len(r_path)):
+            #print('---------------------------considering vehicle ' , j)
+            for k in range(len(r_path[j]) + 1):
+                #print('considering insertion index ' , k)
+                
+                visit = rem_vs[i][0]
+                r_path[j].insert(k,visit)
+
+                cost = PathQuality(r_path,orig_s_times,cData)
+                
+                if cost != None:
+                    f_pts.append((k, j , cost))
+                #else:
+                #    print('infeasible, (point, veh, cost):' , (k, j , cost))
+
+                del r_path[j][k]
+
+
+        #print('------------------------------------------------------------------------------------------------')
+        #print(' RESULTS FOR CUSTOMER  ' , rem_vs[i][0])
+        #print('unsorted feasible points', f_pts)
+        f_pts.sort(key = lambda x:x[2])
+        #print('sorted', f_pts)
+
+        max_min_cost = f_pts[0][2] 
+        if max_min_cost >= max_min_cost_tot:
+            #print('new_max_min_cost found: , ' , max_min_cost)
+            v = rem_vs[i][0]
+            P = f_pts[0:d]
+            max_min_cost_tot = max_min_cost
+
+    return (v, P)
+
+    
+    
+
+def PathQuality(path,orig_s_times,cData):
+    #if a path is feasible, return the objective value for the MPP
+    #if infeasible, return None
+    
+    nVeh = len(orig_s_times)
+    nCus = len(orig_s_times[0])+1
+
+    visits = makeVisitData(cp.deepcopy(path),cData)
+
+    if visits == None:
+        return None
+    else:
+        s_times = GetSTimes(nCus,nVeh,visits)
+        obj = Objective(s_times, orig_s_times)
+
+        return obj
+
+
+
+
+
+def Reinsert(path, rem_vs):
+    #takes a reduced path, visits to reinsert (rem_vs), and a lower bound on the best solution,
+    #Explores the search tree of reinserting all of  the visits, considering only the top d (i.e. 5) choices for each variable based on the heuristic in the paper
+    #the ordering heuristic is to choose the worst variable (i.e. the one with the maximum insertion cost to the objective) then select its best value
+
+    #d is discrepancy limit: set to 5 as per paper
+    d = 5
+    
+
+
+
+
+
+def run_LNS(frac, bPath, pInfo, nSwaps,d):
     #run the LNS algorithm
     #parameters: frac -  The fraction of visits that will be removed and reinserted for each iteration of local search.
     #bPath: benchmarks folder path
@@ -233,19 +348,37 @@ def run_LNS(frac, bPath, pInfo, nSwaps):
     original_path = readPath(original_soln_file,nVeh)
 
     #get a list of parameters for each visit in the routes [[customer number, vehichle served by, service time],...]
-    all_visits = makeVisitData(cp.deepcopy(path),cp.deepcopy(cData))
-    original_visits = makeVisitData(cp.deepcopy(original_path), cp.deepcopy(orig_cData))
+    all_visits = makeVisitData(cp.deepcopy(path),cData)
+    original_visits = makeVisitData(cp.deepcopy(original_path), orig_cData)
+
+    #get initial objective value for MPP
+    orig_s_times = GetSTimes(nCus,nVeh, cp.deepcopy(original_visits))
+    
+    #Get service times from initial solution
+    s_times = GetSTimes(nCus, nVeh, cp.deepcopy(all_visits))
+    
+    best_obj = Objective(cp.deepcopy(s_times),cp.deepcopy(orig_s_times))
+    
+
+
+    print('initial best obj', best_obj)
+
+
 
 
     #get removed visits and the reduced path
-    rem_vs, red_path = removeVisits(cp.deepcopy(path),cp.deepcopy(all_visits),cp.deepcopy(cData),float(frac))
+    rem_vs, red_path = removeVisits(cp.deepcopy(path),cp.deepcopy(all_visits),cData,float(frac))
 
-    print('rem_vs ', rem_vs)
-    print('path-reduced ', red_path)
+    print('runing choosevisit: \n')
+    (v,P) = ChooseVisit(cp.deepcopy(red_path), cp.deepcopy(rem_vs),orig_s_times,cData,d)
+    
+    print('selected variable and top points: ', v, ' \n ', P)
 
     
 
-    #TO DO: check copy for all list args
+    
+
+    #TO DO: check copy for all list args. CData is OK since not modified.
 
     soln = []
     return soln
@@ -260,12 +393,14 @@ if __name__ == '__main__':
     parser.add_argument('--frac', '-f', type=float, default=0.25,
                         help='The fraction of visits that will be removed and reinserted for each iteration of local search.')
     parser.add_argument('--benchmarks-dir', '-b', type=str, required=True,
-                        help='The path of the benchmarks folder')
+                        help='The path of the benchmarks folder. Ex. if in same folder, then .\benchmarks')
     parser.add_argument('--prob-info', '-p', type=str, required=True,
                         help='The details of the problem in the format of the benchmark names, i.e. _v1_c4_tw4_xy16_0')
     parser.add_argument('--nSwaps', '-s', type=str, default=2, help='The number of swaps')
     parser.add_argument('--optimal', '-o', type=str, default = 0,
                         help='the optimal solution, if available. If not provided, valued at 0. ')
+    parser.add_argument('--d', '-d', type=str, default = 5,
+                        help='parameter for limited discrepancy search')
 
     args = parser.parse_args()
 
@@ -274,7 +409,7 @@ if __name__ == '__main__':
 
     else:
         print('running LNS...')
-        soln = run_LNS(args.frac, args.benchmarks_dir, args.prob_info, args.nSwaps)
+        soln = run_LNS(args.frac, args.benchmarks_dir, args.prob_info, args.nSwaps,int(args.d))
 
         print('saving solution...')
         write_Soln(soln)
